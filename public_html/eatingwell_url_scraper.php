92fbe8ee170f2c348400149085bf29f59d8f5051
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-07 15:49:02 -0500

    Auto-commit from vim

--- public_html/eatingwell_url_scraper.php
+++ public_html/eatingwell_url_scraper.php
@@ -18,26 +18,37 @@ for( $i=0; $i< 100; $i++ ){
   $statement = $pdo->prepare("SELECT * FROM EatingWellRecipe WHERE RecipeNumber=?");
   $recipe_id = 261291+$i;
   $statement->execute([$recipe_id]);
-  d($statement->fetch());
-  exit;
-  $crawler = $client->request('GET', "http://www.eatingwell.com/recipe/$recipe_id");
+  $existing = $statement->fetch();
+  d($existing);
+  if( !$existing ){
+    $url = "http://www.eatingwell.com/recipe/$recipe_id";
+    $crawler = $client->request('GET', $url);
 
-  $ingredients = array();
-  $title = "";
-  $crawler->filter('h3[itemprop="name"]')->each(function ($node) {
-    global $title;
-    $title = $node->html();
-  });
-  $crawler->filter('span[itemprop="ingredients"]')->each(function ($node) {
-    global $ingredients;
-    $ingredients[] = $node->html();
-  });
-  d($ingredients,$title);
-  
-  d($crawler
-    ,$crawler->html()
-  );
-  exit;
+    $ingredients = array();
+    $title = "";
+    $crawler->filter('h3[itemprop="name"]')->each(function ($node) {
+      global $title;
+      $title = $node->html();
+    });
+    $crawler->filter('span[itemprop="ingredients"]')->each(function ($node) {
+      global $ingredients;
+      $ingredients[] = $node->html();
+    });
+
+    $r_q = $pdo->prepare( "INSERT INTO EatingWellRecipe (RecipeNumber,RecipeName,Url) VALUES (?,?,?)" );
+    $r_q->execute([$recipe_id,$title,$url]);
+    $r_id = $pdo->insert_id;
+    d($r_id);
+    $i_q = $pdo->prepare( "INSERT INTO EatingWellRecipeIngredient (EatingWellRecipeId,IngredientText) VALUES (?,?)" );
+    foreach( $ingredients as $i ){
+      $i_q->execute([$r_id,$i]);
+    }
+    
+    d($crawler
+      ,$crawler->html()
+    );
+    exit;
+  }
 }
 
 
