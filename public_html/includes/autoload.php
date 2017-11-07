486eddde43185716bf6878875429bfc21cd0e73c
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-07 15:15:22 -0500

    Auto-commit from vim

---
+++ public_html/includes/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once( $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php' );
+
+spl_autoload_register(function ($class_name) {
+  $fname = "{$_SERVER['DOCUMENT_ROOT']}/includes/$class_name.php";
+  if( file_exists($fname) )
+    require_once $fname;
+});
