8baf9c058721970347ebb8a53cc719d254f9ad77
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-07 15:34:10 -0500

    Auto-commit from vim

--- public_html/includes/DbConn.php
+++ public_html/includes/DbConn.php
@@ -1,7 +1,7 @@
 <?php
 
 class DbConn {
-  public static $conn = NULL;
+  public static $pdo = NULL;
   public static $host = '127.0.0.1';
   public static $db = 'recipesub';
   public static $user = 'recipesub';
@@ -9,16 +9,16 @@ class DbConn {
   public static $charset = 'utf8mb4';
 
 
-  public static function getConn(){
+  public static function getPdo(){
     $opt = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     $dsn = "mysql:host=".static::$host.";dbname=".static::$db.";charset=".static::$charset;
-    if( static::$conn == NULL ){
-      static::$conn = new PDO($dsn, static::$user, static::$pass, $opt);
+    if( static::$pdo == NULL ){
+      static::$pdo = new PDO($dsn, static::$user, static::$pass, $opt);
     }
-    return static::$conn;
+    return static::$pdo;
   }
 }
