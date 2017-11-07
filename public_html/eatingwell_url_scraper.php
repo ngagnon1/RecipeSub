e19eb6b6ee2bca1281c30cb42e6cbb008bec3f6f
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-07 14:49:52 -0500

    Auto-commit from vim

--- public_html/eatingwell_url_scraper.php
+++ public_html/eatingwell_url_scraper.php
@@ -15,6 +15,14 @@ $client->setClient($gzl);
 for( $i=0; $i< 100; $i++ ){
   $recipe_id = 261291+$i;
   $crawler = $client->request('GET', "/http://www.eatingwell.com/recipe/$recipe_id");
+
+  $ingredients = array();
+  $crawler->filter('span[itemprop=ingredients]')->each(function ($node) {
+    global $ingredients;
+    $ingredients[] = $node->html();
+  });
+  d($ingredients);
+  
   d($crawler);
   exit;
 }
