237b0d09ba21472b1843471f6a98154b41bb316b
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-07 14:46:26 -0500

    Auto-commit from vim

--- public_html/eatingwell_url_scraper.php
+++ public_html/eatingwell_url_scraper.php
@@ -4,8 +4,22 @@ require_once( $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php' );
 
 use Goutte\Client;
 
-for( $i=1; $i< 100; $i++ ){
+$client = new Client();
+$gzl = new GuzzleHttp\Client( array(
+  'curl' => array(
+    CURLOPT_SSL_VERIFYPEER => false,
+  ),
+) );
+$client->setClient($gzl);
+
+for( $i=0; $i< 100; $i++ ){
+  $recipe_id = 261291+$i;
+  $crawler = $client->request('GET', "/http://www.eatingwell.com/recipe/$recipe_id");
+  d($crawler);
+  exit;
 }
+
+
 
 
 echo 'ff';
