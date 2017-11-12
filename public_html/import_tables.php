b92c53dd74bbee357b472185bc506d1bff321e41
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-12 13:31:03 -0500

    Auto-commit from vim

--- public_html/import_tables.php
+++ public_html/import_tables.php
@@ -1,3 +1,4 @@
 <?php
-$data=file_get_contents("grocery_data/train_samples1.csv");
+      require_once "includes/autoload.php"; 
+$data=file_get_contents("grocery_data/train_sample1.csv");
 echo $data;
