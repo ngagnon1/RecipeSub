e4a043f7d1674d8cd9f55d41fb794bc30729c3d1
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-11-12 14:16:21 -0500

    Auto-commit from vim

--- public_html/import_tables.php
+++ public_html/import_tables.php
@@ -7,7 +7,9 @@ $line = fgetcsv($handle);
 
 $out = array();
 $pdo = DbConn::getPdo();
+
 $cnt = 0;
+$dumbcounter = 0;
 while( $line = fgetcsv($handle) ){
   if( $cnt++ > 0 ){
     $line[1] = "\"".date('Y-m-d',strtotime($line[1]))."\"";
@@ -17,10 +19,14 @@ while( $line = fgetcsv($handle) ){
     $out[] = implode( ", ", $line );
   }
   if( count($out) > 1000 ){
+    $dumbcounter++;
     $sql = "INSERT INTO train_sample1 VALUES ( ".implode( "),(", $out ).")";
+    //d($sql,$out);
+    if ($dumbcounter >1) exit;
     if( $cnt % 10000 == 0 ) echo $cnt."<br/>";
-    $pdo->query($sql)->execute();
+    $pdo->query($sql);//->execute();
     $out = array();
+
   }
 }
 $sql = "INSERT INTO train_sample1 VALUES ( ".implode( "),(", $out ).")";
