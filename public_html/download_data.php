1e6380a065b3e9600cf6ee1757aa526bdd308bb3
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-12-10 09:45:23 -0500

    Auto-commit from vim

--- public_html/download_data.php
+++ public_html/download_data.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once "includes/autoload.php"; 
+
 set_time_limit(30*60);
 
-require_once "includes/autoload.php"; 
 
 $handle = fopen( "grocery_data/train_sample1.csv", "rw" );
 d($handle);
