c08643890dc1d0c8a2f3342056adf88e8b668827
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-12-10 09:45:01 -0500

    Auto-commit from vim

--- public_html/download_data.php
+++ public_html/download_data.php
@@ -1,4 +1,18 @@
 <?php
 
-echo "hello!";
+set_time_limit(30*60);
 
+require_once "includes/autoload.php"; 
+
+$handle = fopen( "grocery_data/train_sample1.csv", "rw" );
+d($handle);
+
+exit;
+
+$pdo = DbConn::getPdo();
+
+$sql = "INSERT INTO train_sample1 VALUES ( ".implode( "),(", $out ).")";
+
+$handle = fopen( "grocery_data/train_sample1.csv", "r" );
+
+fputcsv
