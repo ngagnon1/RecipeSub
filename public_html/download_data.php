b1e690362c252b0d0ee4afd00e2566f1b1ec8d7a
Nathan Gagnon <nathan.gagnon@tfc.com>
2017-12-12 23:26:34 -0500

    Auto-commit from vim

--- public_html/download_data.php
+++ public_html/download_data.php
@@ -4,7 +4,7 @@ require_once "includes/autoload.php";
 
 set_time_limit(30*60);
 
-$handle = fopen( "grocery_data/grocdbkitten.csv", "w" );
+$handle = fopen( "grocery_data/groc_df_v.csv", "w" );
 
 $pdo = DbConn::getPdo();
 
@@ -14,4 +14,9 @@ $sql = "SELECT * FROM Groc_df_v LIMIT $offset, 100";
 
 $fetcher  = $pdo->query($sql);
 
-d($fetcher->fetchAll());
+
+fputcsv( $handle, array("a","b") );
+d(
+  $fetcher->fetchAll()
+  ,file_get_contents( $_SERVER['DOCUMENT_ROOT'].'/grocery_data/groc_df_v.csv' )
+);
