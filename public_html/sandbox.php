25c964e9694e868e182b0044257cd18f3d20f8c4
Nathan <nathancgagnon@gmail.com>
2018-01-15 13:34:41 -0500

    Auto-commit from vim

--- public_html/sandbox.php
+++ public_html/sandbox.php
@@ -1,3 +1,16 @@
 <?php
 
+require_once "includes/autoload.php"; 
+
 echo 'hi 3';
+
+$pdo = DbConn::getPdo();
+
+$sql = "SELECT * FROM Groc_df_v LIMIT 10";
+
+$fetcher  = $pdo->query($sql);
+
+$cnt = 0;
+while( $r = $fetcher->fetch() ){
+  echo implode( ', ', $r )."<br/>";
+}
