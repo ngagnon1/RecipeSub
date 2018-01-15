27641ef600eb66523ed86983e232c438f37ad9cc
Nathan <nathancgagnon@gmail.com>
2018-01-15 14:07:27 -0500

    Auto-commit from vim

--- public_html/sandbox.php
+++ public_html/sandbox.php
@@ -12,7 +12,7 @@ $fetcher  = $pdo->query($sql);
 
 $out = array();
 while( $r = $fetcher->fetch() ){
-  $new = strtolower( trim( preg_replace( ' \+', ' ', preg_replace( '/[^A-Za-z ]/', ' ', $r['PartA'] ) ) ) );
+  $new = strtolower( trim( preg_replace( '/ +/', ' ', preg_replace( '/[^A-Za-z ]/', ' ', $r['PartA'] ) ) ) );
   $out[] = array(
     "orig" => $r['PartA'],
     "new" => $new,
