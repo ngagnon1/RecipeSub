7b62ac72cf2a1fe6b5947579ffbd31074b4173c3
Nathan <nathancgagnon@gmail.com>
2018-01-15 13:48:16 -0500

    Auto-commit from vim

--- public_html/sandbox.php
+++ public_html/sandbox.php
@@ -14,14 +14,16 @@ $cnt = 0;
 $measurements = array();
 $out = array();
 while( $r = $fetcher->fetch() ){
-  $pattern = '/^[0-9]+\s+([\w\-]+).*/';
+  $pattern = '/^[0-9]+\s+([\w\-]+)(.*)/';
   if( preg_match( $pattern, $r['IngredientText'] ) ){
     $measurement = preg_replace( $pattern, '$1', $r['IngredientText'] );
+    $remainder = preg_replace( $pattern, '$2', $r['IngredientText'] );
     if( !in_array($measurement,$measurements) ){
       $measurements[] = $measurement;
       $out[] = array(
         "original" => $r['IngredientText'],
         "extract" => $measurement,
+        "remainder" => $remainder,
       );
     }
   }
