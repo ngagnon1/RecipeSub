51a237bf142017f138c050662d7637e434321511
Nathan <nathancgagnon@gmail.com>
2018-01-15 13:40:41 -0500

    Auto-commit from vim

--- public_html/sandbox.php
+++ public_html/sandbox.php
@@ -6,11 +6,17 @@ echo 'hi 3';
 
 $pdo = DbConn::getPdo();
 
-$sql = "SELECT * FROM EatingWellRecipeIngredient LIMIT 10";
+$sql = "SELECT * FROM EatingWellRecipeIngredient LIMIT 1000";
 
 $fetcher  = $pdo->query($sql);
 
 $cnt = 0;
+$measurements = array();
 while( $r = $fetcher->fetch() ){
-  echo implode( ', ', $r )."<br/>";
+  $pattern = '/^[0-9]+\s+([\w\-]+).*/';
+  if( preg_match( $pattern, $r['IngredientText'] ) ){
+    $measurement = preg_replace( $pattern, '$1', $r['IngredientText'] );
+    echo $measurement;
+    exit;
+  }
 }
